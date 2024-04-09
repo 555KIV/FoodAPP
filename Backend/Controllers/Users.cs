21ad2e0ebf12a308c7f558eb6c21dba453811cad
555KIV <a555kiv.master2015@yandex.ru>
2024-04-09 18:24:35 +0300

    Initial commit

---
+++ Backend/Controllers/Users.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+
+[ApiController]
+[Route("api/users")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = new[]
+        {
+            new { Name = "Oleg" },
+            new { Name = "Ivan" }
+        };
+        return Ok(users);
+    }
+}
