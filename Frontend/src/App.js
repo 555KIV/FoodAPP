1a6910363395b64fcac92c1878b08d2267293e2e
Andrew <andrey_U_O@mail.ru>
2024-04-12 01:07:16 +0300

    test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
 
@@ -11,20 +10,19 @@ class App extends Component {
   }
 
   getUsers = async () => {
-    var response = await fetch(
+    const response = await fetch(
       'api/users',
       {
         method: 'GET'
       }
     )
 
-    var responsejson = await response.json();
-    this.setState({users: responsejson})
+    const responseJSON = await response.json();
+    this.setState({users: responseJSON})
   }
 
   render() {
     const users = this.state.users.map((item,index) => <li key={index}>{item.name}</li>)
-
     return (
       <div className="App">
         <button onClick={this.getUsers}>Загрузка пользователей</button>
